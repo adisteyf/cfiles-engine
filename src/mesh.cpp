da9cf577bfc6104b437f2cd777438ff1d58aeb66
adisteyf <adisteyf@gmail.com>
2025-01-08 22:28:50 +0300

    fixed vao (ex.)

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -1,41 +1,39 @@
 #include "mesh.h"
 
-Mesh::Mesh(std::vector<Vertex> &vertices, std::vector<GLuint> &indices, std::vector<Texture> &textures)
+Mesh::Mesh(std::vector <Vertex>& vertices, std::vector <GLuint>& indices, std::vector <Texture>& textures)
 {
     Mesh::vertices = vertices;
     Mesh::indices = indices;
     Mesh::textures = textures;
 
     vao.bind();
-    fe_debug();
-
     VBO vbo(vertices);
     EBO ebo(indices);
 
-    vao.makeAttrib(vbo, 0, 3, GL_FLOAT, 11*sizeof(float), (void*)0);
-    vao.makeAttrib(vbo, 1, 3, GL_FLOAT, 11*sizeof(float), (void*)(3 * sizeof(float)));
-    vao.makeAttrib(vbo, 2, 3, GL_FLOAT, 11*sizeof(float), (void*)(6 * sizeof(float)));
-    vao.makeAttrib(vbo, 3, 2, GL_FLOAT, 11*sizeof(float), (void*)(9 * sizeof(float)));
-    
+    vao.makeAttrib(vbo, 0, 3, GL_FLOAT, sizeof(Vertex), (void*)0);
+    vao.makeAttrib(vbo, 1, 3, GL_FLOAT, sizeof(Vertex), (void*)(3 * sizeof(float)));
+    vao.makeAttrib(vbo, 2, 3, GL_FLOAT, sizeof(Vertex), (void*)(6 * sizeof(float)));
+    vao.makeAttrib(vbo, 3, 2, GL_FLOAT, sizeof(Vertex), (void*)(9 * sizeof(float)));
+
     vao.unbind();
     vbo.unbind();
     ebo.unbind();
 }
 
+Mesh::~Mesh() {}
 void Mesh::draw
 (
-    Shader &shader,
-    Camera &camera,
+    Shader& shader,
+    Camera& camera,
     glm::mat4 matrix,
     glm::vec3 translation,
     glm::quat rotation,
     glm::vec3 scale
 )
-
 {
     shader.bind();
     std::cout << glGetError() << std::endl;
-    vao.bind(); // error here
+    vao.bind();
     std::cout << glGetError() << std::endl;
 
     unsigned int numDiffuse = 0;
@@ -43,23 +41,19 @@ void Mesh::draw
 
     for (unsigned int i = 0; i < textures.size(); i++)
     {
-        std::string number;
+        std::string num;
         std::string type = textures[i].type;
-
         if (type == "diffuse")
         {
-            number = std::to_string(numDiffuse++);
+            num = std::to_string(numDiffuse++);
         }
         else if (type == "specular")
         {
-            number = std::to_string(numSpecular++);
+            num = std::to_string(numSpecular++);
         }
-
-		textures[i].texUnit(shader, (type + number).c_str(), i);
-		textures[i].bind();
+        textures[i].texUnit(shader, (type + num).c_str(), i);
+        textures[i].bind();
     }
-
-    std::cout << "next is camPos uniform" << std::endl;
     glUniform3f(glGetUniformLocation(shader.getProgram(), "camPos"), camera.pos.x, camera.pos.y, camera.pos.z);
     camera.matrix(shader, "camMatrix");
 
@@ -71,18 +65,12 @@ void Mesh::draw
     rot = glm::mat4_cast(rotation);
     sca = glm::scale(sca, scale);
 
-    std::cout << "next is translation" << std::endl;
     glUniformMatrix4fv(glGetUniformLocation(shader.getProgram(), "translation"), 1, GL_FALSE, glm::value_ptr(trans));
     glUniformMatrix4fv(glGetUniformLocation(shader.getProgram(), "rotation"), 1, GL_FALSE, glm::value_ptr(rot));
     glUniformMatrix4fv(glGetUniformLocation(shader.getProgram(), "scale"), 1, GL_FALSE, glm::value_ptr(sca));
     glUniformMatrix4fv(glGetUniformLocation(shader.getProgram(), "model"), 1, GL_FALSE, glm::value_ptr(matrix));
 
+    std::cout << "??: " << glGetError() << std::endl;
     glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
-    std::cout << glGetError() << std::endl;
-    std::cout << "end of mesh draw" << std::endl;
-}
-
-Mesh::~Mesh()
-{
-    vao.free();
+    std::cout << "glDrawElements(): " << glGetError() << std::endl;
 }
--- src/window.cpp
+++ src/window.cpp
@@ -41,6 +41,7 @@ void Window::pollEvents() {
 }
 
 void Window::clear() {
+    glClearColor(0.07f, 0.13f, 0.17f, 1.f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
