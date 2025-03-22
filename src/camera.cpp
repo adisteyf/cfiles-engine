e5507bbe20175e949ed2814f983c66d7be30c1a3
Adisteyf <adisteyf@gmail.com>
2025-03-22 17:57:06 +0300

    better camera class again

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,13 +1,13 @@
 #include "camera.h"
 
 // TODO: add lastMouseX/Y and use it instead glfwSetCursorPos()
-void Camera::updateMatrix(float FOVdeg, float nearPlane, float farPlane)
+void Camera::updateMatrix(void)
 {
     glm::mat4 view = glm::mat4(1.0f);
     glm::mat4 projection = glm::mat4(1.0f);
 
     view = glm::lookAt(pos, pos + orientation, up);
-    projection = glm::perspective(glm::radians(FOVdeg), (float)w / (float)h, nearPlane, farPlane);
+    projection = glm::perspective(fov, (float)w / (float)h, near_plane, far_plane);
     cameraMatrix = projection * view;
 }
 
